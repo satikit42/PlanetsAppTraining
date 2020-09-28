4a04672f6bf622160c888f9fa29fe270bd3f0cbb
Daniel Costa <cmtedbcosta@gmail.com>
2020-09-28 16:49:18 +0200

    Remove unused code in test

--- app/src/test/java/com/example/planetsapptraining/repositories/PlanetRepositoryImplTest.kt
+++ app/src/test/java/com/example/planetsapptraining/repositories/PlanetRepositoryImplTest.kt
@@ -1,14 +1,15 @@
 package com.example.planetsapptraining.repositories
 
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.example.planetsapptraining.Planet
-import io.mockk.*
+import io.mockk.coEvery
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.mockkClass
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Assert.assertEquals
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 
 @ExperimentalCoroutinesApi
@@ -16,20 +17,17 @@ class PlanetRepositoryImplTest {
 
     lateinit var repository: PlanetRepositoryImpl
     lateinit var service: PlanetService
-
     private val testDispatcher = TestCoroutineDispatcher()
-
-    @get:Rule
-    val instantExecutorRule = InstantTaskExecutorRule()
-
     private val planetAmount = 10
 
     @Before
     fun setUp() {
         service = mockk {
-            coEvery { getPlanets() } returns (1..planetAmount).map { mockkClass(PlanetResponse::class) {
-                every { mapToDomain() } returns mockkClass(Planet::class)
-            } }
+            coEvery { getPlanets() } returns (1..planetAmount).map {
+                mockkClass(PlanetResponse::class) {
+                    every { mapToDomain() } returns mockkClass(Planet::class)
+                }
+            }
         }
         repository = PlanetRepositoryImpl(service, testDispatcher)
     }
